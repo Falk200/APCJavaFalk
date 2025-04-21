d30eddc02220b17b3e11f2820b946cd1e5037aeb
Falk A. <fassion@smail.uni-koeln.de>
2025-04-21 15:26:09 +0200

    Added Publication, Book and Textbook classes

---
+++ Homework/src/main/java/org/example/Textbook.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Textbook extends Book {
+    private String subject;
+
+    @Override
+    public String getInfo(){
+        return super.getInfo()+" Subject: "+subject;
+    }
+
+    public Textbook(String title,int year, String author, String subject){
+       super(title, year, author);
+       this.subject = subject;
+    }
+}
